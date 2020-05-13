090f36f89030cc58ac90575f649404e19fcec6f3
chenwenkang <864730420@qq.com>
2020-05-13 18:03:52 +0800

    Top10商品的Top10SessionID统计 并且入数据

--- session/src/main/scala/SessionStat.scala
+++ session/src/main/scala/SessionStat.scala
@@ -16,6 +16,7 @@ import scala.util.Random
 object SessionStat {
 
 
+
   def main(args: Array[String]): Unit = {
 
     //获取筛选条件
@@ -86,10 +87,109 @@ object SessionStat {
     }
 
 
-    top10PopularCategories(sparkSession, taskUUID, sessionId2ActionRDD)
+  val top10CategoryArray =  top10PopularCategories(sparkSession, taskUUID, sessionId2ActionRDD)
+
+    top10ActiveSession(sparkSession,taskUUID,sessionIdFilterActionRDD,top10CategoryArray)
+
+
 
   }
 
+  def top10ActiveSession(sparkSession: SparkSession,
+                         taskUUID: String,
+                         sessionId2FilterActionRDD: RDD[(String, UserVisitAction)],
+                         top10CategoryArray: Array[(SortKey, String)]): Unit = {
+    // 第一步：过滤出所有点击过Top10品类的action
+    // 1： join
+    //    val cid2CountInfoRDD = sparkSession.sparkContext.makeRDD(top10CategoryArray).map{
+    //      case (sortKey, countInfo) =>
+    //        val cid = StringUtils.getFieldFromConcatString(countInfo, "\\|", Constants.FIELD_CATEGORY_ID).toLong
+    //        (cid, countInfo)
+    //    }
+    //
+    //      val cid2ActionRDD = sessionId2FilterActionRDD.map{
+    //        case (sessionId, action) =>
+    //          val cid = action.click_category_id
+    //          (cid, action)
+    //      }
+    //
+    //    val sessionId2ActionRDD = cid2CountInfoRDD.join(cid2ActionRDD).map{
+    //      case (cid, (countInfo, action)) =>
+    //        val sid = action.session_id
+    //        (sid, action)
+    //    }
+
+    // 2：使用filter
+    // cidArray: Array[Long] 包含了Top10热门品类ID
+    val cidArray = top10CategoryArray.map{
+      case (sortKey, countInfo) =>
+        val cid = StringUtils.getFieldFromConcatString(countInfo, "\\|", Constants.FIELD_CATEGORY_ID).toLong
+        cid
+    }
+
+    // 所有符合过滤条件的，并且点击过Top10热门品类的action
+    val sessionId2ActionRDD = sessionId2FilterActionRDD.filter{
+      case (sessionId, action) =>
+        cidArray.contains(action.click_category_id)
+    }
+
+    // 按照sessionId进行聚合操作
+    val sessionId2GroupRDD = sessionId2ActionRDD.groupByKey()
+
+    // cid2SessionCountRDD: RDD[(cid, sessionCount)]
+    val cid2SessionCountRDD = sessionId2GroupRDD.flatMap{
+      case (sessionId, iterableAction) =>
+
+        val categoryCountMap = new mutable.HashMap[Long, Long]()
+
+        for(action <- iterableAction){
+          val cid = action.click_category_id
+          if(!categoryCountMap.contains(cid))
+            categoryCountMap += (cid->0)
+          categoryCountMap.update(cid, categoryCountMap(cid) + 1)
+        }
+
+        // 记录了一个session对于它所有点击过的品类的点击次数
+        for((cid, count) <- categoryCountMap)
+          yield (cid, sessionId + "=" + count)
+    }
+
+    // cid2GroupRDD: RDD[(cid, iterableSessionCount)]
+    // cid2GroupRDD每一条数据都是一个categoryid和它对应的所有点击过它的session对它的点击次数
+    val cid2GroupRDD = cid2SessionCountRDD.groupByKey()
+
+    // top10SessionRDD: RDD[Top10Session]
+    val top10SessionRDD = cid2GroupRDD.flatMap{
+      case (cid, iterableSessionCount) =>
+        // true: item1放在前面
+        // flase: item2放在前面
+        // item: sessionCount   String   "sessionId=count"
+        val sortList = iterableSessionCount.toList.sortWith((item1, item2) => {
+          item1.split("=")(1).toLong > item2.split("=")(1).toLong
+        }).take(10)
+
+        val top10Session = sortList.map{
+          // item : sessionCount   String   "sessionId=count"
+          case item =>
+            val sessionId = item.split("=")(0)
+            val count = item.split("=")(1).toLong
+            Top10Session(taskUUID, cid, sessionId, count)
+        }
+
+        top10Session
+    }
+
+    import sparkSession.implicits._
+    top10SessionRDD.toDF().write
+      .format("jdbc")
+      .option("url", ConfigurationManager.config.getString(Constants.JDBC_URL))
+      .option("user", ConfigurationManager.config.getString(Constants.JDBC_USER))
+      .option("password", ConfigurationManager.config.getString(Constants.JDBC_PASSWORD))
+      .option("dbtable", "top10_session_0308")
+      .mode(SaveMode.Append)
+      .save()
+
+  }
 
   def getClickCount(sessionIdFilterActionRDD: RDD[(String, UserVisitAction)]) = {
 
@@ -151,7 +251,7 @@ object SessionStat {
 
 
   def top10PopularCategories(sparkSession: SparkSession, taskUUID: String,
-                             sessionIdFilterActionRDD: RDD[(String, UserVisitAction)]): Unit = {
+                             sessionIdFilterActionRDD: RDD[(String, UserVisitAction)]) = {
 
     //第一步：获取所有发生过点击，下单，付款的品类
 
@@ -243,7 +343,7 @@ object SessionStat {
       .mode(SaveMode.Append)
       .save()
 
-
+    top10CategoryArray
   }
 
   def getFullCount(cid2CidRDD: RDD[(Long, Long)], cid2ClickCountRDD: RDD[(Long, Long)],
