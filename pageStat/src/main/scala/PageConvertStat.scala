c286709622229d2f55e017fe31fd28434bc50e56
chenwenkang <864730420@qq.com>
2020-05-18 17:46:28 +0800

    页面单跳转化率完成写入数据库

--- pageStat/src/main/scala/PageConvertStat.scala
+++ pageStat/src/main/scala/PageConvertStat.scala
@@ -7,7 +7,9 @@ import commons.utils.{DateUtils, ParamUtils}
 import net.sf.json.JSONObject
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SaveMode, SparkSession}
+
+import scala.collection.mutable
 
 
 object PageConvertStat {
@@ -45,8 +47,8 @@ object PageConvertStat {
     //pageFlowArray.tail: [2,3,4,5,6,7]
     //pageFlowArray.slice(0, pageFlowArray.length - 1).zip(pageFlowArray.tail):[(1,2),(2,3)....]
     //targetPageSplit:[1_2,2_3,3_4,......]
-    val targetPageSplit = pageFlowArray.slice(0, pageFlowArray.length - 1).zip(pageFlowArray.tail).map{
-      case (page1,page2) => page1 + "_" + page2
+    val targetPageSplit = pageFlowArray.slice(0, pageFlowArray.length - 1).zip(pageFlowArray.tail).map {
+      case (page1, page2) => page1 + "_" + page2
     }
 
 
@@ -102,13 +104,59 @@ object PageConvertStat {
 
     val startPage = pageFlowArray(0).toLong
 
-   val startPageCount =  sessionId2ActionRDD.filter{
-      case (sessionId,action) => action.page_id ==  startPage
+    val startPageCount = sessionId2ActionRDD.filter {
+      case (sessionId, action) => action.page_id == startPage
     }.count()
 
 
+    getPageConvert(sparkSession, taskUUID, targetPageSplit, startPageCount, pageSplitCountMap)
+
 
   }
+
+
+
+  def getPageConvert(sparkSession: SparkSession,
+                     taskUUID: String, targetPageSplit: Array[String],
+                     startPageCount: Long,
+                     pageSplitCountMap: collection.Map[String, Long]): Unit = {
+
+    val pageSplitRatio = new mutable.HashMap[String, Double]()
+    var lastPageCount = startPageCount.toDouble
+
+    //1,2,3,4,5,6,7
+    //1_2,2_3,.....
+    for (pageSplit <- targetPageSplit) {
+      //第一次循环：1_2 count
+      val currentPageSplitCount = pageSplitCountMap.get(pageSplit).get.toDouble
+      val ratio = currentPageSplitCount / lastPageCount
+
+      pageSplitRatio.put(pageSplit,ratio)
+      lastPageCount = currentPageSplitCount
+    }
+
+
+    val converStr = pageSplitRatio.map{
+      case (pageSplit,ratio) => pageSplit+ "=" + ratio
+
+
+    }.mkString("|")
+    val pageSplit  = PageSplitConvertRate(taskUUID, converStr)
+
+    val  pageSplitRatioRDD = sparkSession.sparkContext.makeRDD(Array(pageSplit))
+
+    import sparkSession.implicits._
+
+    pageSplitRatioRDD.toDF().write
+      .format("jdbc")
+      .option("url", ConfigurationManager.config.getString(Constants.JDBC_URL))
+      .option("user", ConfigurationManager.config.getString(Constants.JDBC_USER))
+      .option("password", ConfigurationManager.config.getString(Constants.JDBC_PASSWORD))
+      .option("dbtable", "page_split_convert_rate_0308")
+      .mode(SaveMode.Append)
+      .save()
+  }
+
 
   def getUserVisitAction(sparkSession: SparkSession, taskParam: JSONObject)= {
 
