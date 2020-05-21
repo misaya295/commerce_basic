bc20acafe698dc0ed5c06c1136e9b937e02a2337
chenwenkang <864730420@qq.com>
2020-05-21 17:54:17 +0800

    地域商品top3完成

--- areaStat/src/main/scala/AreaTop3Stat.scala
+++ areaStat/src/main/scala/AreaTop3Stat.scala
@@ -6,7 +6,7 @@ import commons.utils.ParamUtils
 import net.sf.json.JSONObject
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SaveMode, SparkSession}
 
 
 object AreaTop3Stat {
@@ -39,6 +39,8 @@ object AreaTop3Stat {
     getAreaPidBasicTable(sprakSession, cityId2PidRDD, cityId2AreaInfoRDD)
 
 
+
+
     sprakSession.udf.register("concat_long_string",(v1:Long,v2:String,split:String) =>
     {
      v1 + split + v2
@@ -49,9 +51,89 @@ object AreaTop3Stat {
 
     getAreaProductClickCountTable(sprakSession)
 
-    sprakSession.sql("select * from tmp_area_click_count").show()
+    sprakSession.udf.register("get_json_field",(json:String,field:String) => {
+
+
+      val jsonObject = JSONObject.fromObject(json)
+      jsonObject.getString(field)
+
+    })
+
+    getAreaProductClickCountInfo(sprakSession)
+
+
+    getTop3Product(sprakSession,taskUUID)
+
+
 
   }
+
+
+  def getTop3Product(sprakSession: SparkSession, taskUUID: String) = {
+
+//
+//    val sql = "select area,city_infos,pid,product_name,product_status,click_count,"+
+//    "row_number() over(PARTITION BY area ORDER BY click_count DESC) rank from tmp_area_count_product_info"
+//
+//
+//    sprakSession.sql(sql).createOrReplaceTempView("temp_test")
+
+
+    val sql = "select area, " +
+      "CASE " +
+      "WHEN area='华北' OR area='华东' THEN 'A_Level' " +
+      "WHEN area='华中' OR area='华南' THEN 'B_Level' " +
+      "WHEN area='西南' OR area='西北' THEN 'C_Level' " +
+      "ELSE 'D_Level' " +
+      "END area_level, " +
+      "city_infos, pid, product_name, product_status, click_count from (" +
+      "select area, city_infos, pid, product_name, product_status, click_count, " +
+      "row_number() over(PARTITION BY area ORDER BY click_count DESC) rank from " +
+      "tmp_area_count_product_info) t where rank<=3"
+
+
+       val top3ProductRDD = sprakSession.sql(sql).rdd.map {
+
+          case row =>
+            AreaTop3Product(taskUUID,row.getAs[String]("area"),row.getAs[String]("area_level"),
+              row.getAs[Long]("pid"),row.getAs[String]("city_infos"),
+              row.getAs[Long]("click_count"),row.getAs[String]("product_name"),
+              row.getAs[String]("product_status")
+            )}
+
+
+    import sprakSession.implicits._
+    top3ProductRDD.toDF().write
+      .format("jdbc")
+      .option("url", ConfigurationManager.config.getString(Constants.JDBC_URL))
+      .option("user", ConfigurationManager.config.getString(Constants.JDBC_USER))
+      .option("password", ConfigurationManager.config.getString(Constants.JDBC_PASSWORD))
+      .option("dbtable", "area_top_product")
+      .mode(SaveMode.Append)
+      .save()
+  }
+
+
+  def getAreaProductClickCountInfo(sprakSession: SparkSession) = {
+
+    //tmp_area_click_count: area,city_infos,pid,click_count tacc
+    //product_info:product_id,product_name,extend_info pi
+
+    val sql = "select tacc.area, tacc.city_infos, tacc.pid, pi.product_name, " +
+      "if(get_json_field(pi.extend_info, 'product_status')='0','Self','Third Party') product_status," +
+      "tacc.click_count " +
+      " from tmp_area_click_count tacc join product_info pi on tacc.pid = pi.product_id"
+
+
+
+    sprakSession.sql(sql).createOrReplaceTempView("tmp_area_count_product_info")
+
+
+
+  }
+
+
+
 
   def getAreaProductClickCountTable(sparkSession: SparkSession) = {
 
